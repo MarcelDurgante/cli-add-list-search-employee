6f0affd6905bfe66dc52d6538e79df3e90dba6b9
MarcelDurgante <marceldur@gmail.com>
2023-08-15 12:37:58 +0200

    dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,8 @@
+// string
+
+let myName = "David";
+let upperCaseMyName = myName.toUpperCase(); 
+
+console.log(upperCaseMyName);  // output: DAVID
+
+// Primitives are imutable, but in the above, the toUpperCase method is creating a new string and it is passing the name to the new variable upperCaseMyName.
