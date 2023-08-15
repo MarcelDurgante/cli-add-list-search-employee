94869baa05c7e035e2e307511ec0ec6a539c575d
MarcelDurgante <marceldur@gmail.com>
2023-08-15 12:38:25 +0200

    variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+// Assigning variables values
+
+let firstName = "David";
+let lastName = "Tucker";
+let numYearsEmployment = 6;
+let dateBirth = new Date("January 1, 1982");
+
+// Changing a variable created with let
+numYearsEmployment = 7;  // OK
+
+// Using const to create a constant
+const title = "VP of Engineering";
+title = "CTO"; // NOT OK. This would cause an error.
+
+// "Var" exists but don't use it
