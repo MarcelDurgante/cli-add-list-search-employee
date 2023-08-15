f08dbb54590af5f06526d9c79a8cf8af9acf6de6
MarcelDurgante <marceldur@gmail.com>
2023-08-15 10:34:24 +0200

    comments

---
+++ .vscode/comments.js
@@ -0,0 +1,14 @@
+// DOCUMENT AND CONFIGURATION
+
+/**
+ * Joins the first and last names together.
+ * 
+ * @param {string} firstName The first name
+ * @param {string} lastName The last name
+ * @return {string}  The full name
+ */
+function geFullName(firstaName, lastName) {
+    return `${firstaName} ${lastName}`
+};
+
+let lastName = "Marcel"; // eslint-disable-line
