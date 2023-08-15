9d0085ba6989742cb670b7e6db4319deac407cda
MarcelDurgante <marceldur@gmail.com>
2023-08-15 13:37:37 +0200

    boolean basics

---
+++ booleans.js
@@ -0,0 +1,23 @@
+// Defining booleans
+
+let trueValue = true;
+console.log(trueValue);
+
+let falseValue = false;
+console.log(falseValue);
+
+// Using the NOT operator with Booleans
+
+let notTrue = !true;
+console.log(notTrue);
+let notFalse = !false;
+console.log(notFalse);
+
+/* outputs:
+
+true
+false
+false
+true
+
+ */
