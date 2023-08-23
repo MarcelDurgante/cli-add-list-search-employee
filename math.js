7b47c534c3290d66e4eec91b39bc763d5d946896
MarcelDurgante <marceldur@gmail.com>
2023-08-23 02:27:47 +0200

    Mathematical operators

---
+++ math.js
@@ -0,0 +1,44 @@
+// Addition
+let val1 = 3 + 2;
+console.log(`val1: ${val1}`); // val1: 5
+let val2 = 2 + -1;
+console.log(`val2: ${val2}`); // val2: 1
+
+// Subtraction
+let val3 = 3 - 1;
+console.log(`val3: ${val3}`); // val3: 2
+let val4 = 1 - -1;
+console.log(`val4: ${val4}`); // val4: 2
+
+// Division
+let val5 = 3 / 1;
+console.log(`val5: ${val5}`); // val5: 3
+let val6 = 21 / 9;
+console.log(`val6: ${val6}`); // val6: 2.3333333333333335
+
+// Multiplication
+let val7 = 3 * 2;
+console.log(`val7: ${val7}`); // val7: 6
+let val8 = 3 * 3;
+console.log(`val8: ${val8}`); // val8: 9
+
+// Remainder
+let val9 = 9 % 2;
+console.log(`val9: ${val9}`); // val9: 1
+let val10 = -10 % 7;
+console.log(`val10: ${val10}`); // val10: -3
+
+// Exponentiation
+let val11 = 3 ** 2;
+console.log(`val11: ${val11}`); // val11: 9
+let val12 = 2 ** -1;
+console.log(`val12: ${val12}`); // val12: 0.5
+
+// Increment and decrement
+let val13 = 1;
+++val13;
+console.log(`val13: ${val13}`); // val13: 2
+
+let val14 = 1;
+--val14;
+console.log(`val14: ${val14}`); // val14: 0
