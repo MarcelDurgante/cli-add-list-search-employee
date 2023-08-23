1f357ac8dd7a6a63c12eb3382485f3dfa2d7a24f
MarcelDurgante <marceldur@gmail.com>
2023-08-23 05:57:25 +0200

    Fixed issues so app prompts work in terminal

--- project/index.js
+++ project/index.js
@@ -1,2 +1,7 @@
+// The third-party lib 'prompt-sync' allow us to ask the user for inputs.
 import createPrompt from 'prompt-sync';
 let prompt = createPrompt();
+
+let employee = {};
+
+let firstName = prompt("First Name: ");
