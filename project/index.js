b67f5c505fef106c184d05ca13440f0a3acea5d4
MarcelDurgante <marceldur@gmail.com>
2023-08-23 06:14:49 +0200

    Get and check month input valid and in range

--- project/index.js
+++ project/index.js
@@ -24,13 +24,26 @@ employee.lastName = lastName;
 let startDateYear = prompt("Employee Start Year (1990-2023): ");
 // try to convert the string into a number
 startDateYear = Number(startDateYear);
-// CAfter converting, check if it is a valid integer (if it was possible to convert into a number line 26 it should be a valid integer)
+// After converting, check if it is a valid integer (if it was possible to convert into a number line 26 it should be a valid integer)
 if (!Number.isInteger(startDateYear)) {
     console.error(`Enter a valid start date year`);
     process.exit(1);
 }
-// Check if the number is in the range
+// Check if the number is in the year range
 if (startDateYear < 1990 || startDateYear > 2023) {
     console.error(`Enter a start date year within the correct range`);
+    process.exit(1);
+}
+
+let startDateMonth = prompt("Employee Start Date Month (1-12): ");
+startDateMonth = Number(startDateMonth);
+// Check if it is a valid integer again
+if (!Number.isInteger(startDateMonth)) {
+    console.error(`Enter a valid start date month`);
+    process.exit(1);
+}
+// Check if the number is in month range
+if (startDateMonth < 1 || startDateMonth > 12) {
+    console.error(`Enter a start date month within the correct range`);
     process.exit(1);
 }
