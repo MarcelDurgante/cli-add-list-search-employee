13f04e31361ea612a1595444d5ef291234db2055
MarcelDurgante <marceldur@gmail.com>
2023-08-23 06:03:54 +0200

    Capturing firstName works

--- project/index.js
+++ project/index.js
@@ -5,3 +5,11 @@ let prompt = createPrompt();
 let employee = {};
 
 let firstName = prompt("First Name: ");
+// checks if input is not an empty string. 
+if (!firstName) {
+    console.error(`Invalid first name`);
+    // exits the app with 1 code which is an error code instead of passing just zero.
+    process.exit(1);
+}
+// If the above passes we add firstName to the employee object
+employee.firstName = firstName;
