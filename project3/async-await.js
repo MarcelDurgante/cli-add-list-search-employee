ed15eb8f18f52f8a4f2ae2f0c3d94e7f23bbcc6a
MarcelDurgante <marceldur@gmail.com>
2023-09-11 14:31:50 +0200

    Handling error with try catch blocks

--- project3/async-await.js
+++ project3/async-await.js
@@ -8,13 +8,27 @@ import fs from 'node:fs/promises';
 
 // to utilize async and await, we need to have an async function. So here, I'm going to define an async function called loadData.
 async function loadData() {
-    // using the await keyword we'll say that data = await and then fs.readFile, and then we'll pass in the same arguments that we used previously.  We're still using the promise‑based API, so we're still working with promises. The await keyword specifies that the promise will need to complete, and then after that, it will execute the next steps that are listed in your code. This makes it work in many ways similar to how we work with synchronous code. Async has to return a promise and in this case, using await is basically doing that for us behind the scenes so we don't have to do it manually.
-    const data = await fs.readFile('data.json', 'utf-8');
-    // parse this out to be an actual data object by parsing the JSON.
-    const dataObj = JSON.parse(data);
-    // log to the console.
-    console.log(dataObj);
-    console.log('Completed');
+
+    // we need to be able to handle situations where errors may occur. Now the difference here is we're going to go back to what we were using with synchronous code to be able to do this, and that is going to be a try catch block. So here, we're going to specify our try block
+    try {
+
+        // using the await keyword we'll say that data = await and then fs.readFile, and then we'll pass in the same arguments that we used previously.  We're still using the promise‑based API, so we're still working with promises. The await keyword specifies that the promise will need to complete, and then after that, it will execute the next steps that are listed in your code. This makes it work in many ways similar to how we work with synchronous code. Async has to return a promise and in this case, using await is basically doing that for us behind the scenes so we don't have to do it manually.
+        const data = await fs.readFile('data.json', 'utf-8');
+
+        // parse this out to be an actual data object by parsing the JSON.
+        const dataObj = JSON.parse(data);
+
+        // log to the console.
+        console.log(dataObj);
+        console.log('Completed');
+
+        // And then we need to add in our logic for our catch block.
+    } catch (err) {
+        // log out a message...
+        console.log('Could not load and parse file');
+        // ... and then we will throw the error.
+        throw err;
+    };
 }
 
 // call loadData, and then include a final logging statement that says that that promise has completed.
