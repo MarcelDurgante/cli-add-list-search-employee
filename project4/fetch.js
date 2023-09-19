6f09c2ac518d504bae96ccee1f37db765c793b88
MarcelDurgante <marceldur@gmail.com>
2023-09-19 21:52:02 +0200

    Cleaned up code comments.

--- project4/fetch.js
+++ project4/fetch.js
@@ -1,21 +1,4 @@
 import fetch from 'cross-fetch';
-/* const myHeaders = new Headers();
-myHeaders.append("access_key", "f31a79670d602eaa6aa146bb8e632945")
-
-const requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-};
-
-try {
-    const result = await fetch("http://api.exchangeratesapi.io/v1/latest", requestOptions);
-    const resultObj = await result.json();
-    console.log(JSON.stringify(resultObj, null, 2));
-} catch (err) {
-    console.error('Could not fetch currency data');
-    throw err;
-}; */
 
 var requestOptions = {
     method: 'GET',
@@ -25,7 +8,6 @@ var requestOptions = {
 try {
     const result = await fetch("http://api.exchangeratesapi.io/v1/latest?access_key=f31a79670d602eaa6aa146bb8e632945", requestOptions);
     const resultObj = await result.json();
-    console.log('=========================================');
     console.log(JSON.stringify(resultObj, null, 2));
 } catch (err) {
     console.error('Could not fetch currency data');
